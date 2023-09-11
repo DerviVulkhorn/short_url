95252ef7828d1a74cb5db10430d31e28bb6a7901
dervi1999 <dervi.dark@gmail.com>
2023-09-11 11:36:14 +0300

    Аштфд

--- ShortLink/Controllers/MoveUrl.cs
+++ ShortLink/Controllers/MoveUrl.cs
@@ -1,13 +1,35 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ShortLink.Entity;
+using ShortLink.Models;
 
 namespace ShortLink.Controllers
 {
     public class MoveUrl : Controller
     {
-        [HttpGet("{value}")]
+        private readonly EntityDb context;
+        public MoveUrl(EntityDb appDbContext)
+        {
+            this.context = appDbContext;
+        }
+
+        [HttpGet("/s/{value}")]
         public IActionResult Move(string value)
         {
-            return Ok(value);
+            var code = context.codes.FirstOrDefault(u=>u.code== value);
+            var originalLink = context.originalLinks
+            .FirstOrDefault(link => link.code.Any(c => c.id == code.id));
+            if (originalLink != null)
+            {
+                code.countMove++;
+                context.SaveChanges();
+                return Redirect(originalLink.link);
+            }
+            else
+            {
+                return Redirect("/home");
+            }
+            
         }
     }
 }
